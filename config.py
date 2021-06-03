db4206b5099a2a50cfe037428f3d68b9ee53115c
Swapnil-Singh99 <swapnilskumars@gmail.com>
2021-06-03 19:12:56 +0530

    config.py ...

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 
 firebaseConfig = {
-    "apiKey": str(os.environ[apikey]),
-    "authDomain": str(os.environ[authd]),
-    "databaseURL": str(os.environ[dataurl]),
-    "projectId": str(os.environ[projectid]),
-    "storageBucket": str(os.environ[storagebkt]),
-    "messagingSenderId": str(os.environ[messagesendid]),
-    "appId": str(os.environ[appid]),
-    "measurementId": str(os.environ[measureid])
+    "apiKey": str(os.environ['apikey']),
+    "authDomain": str(os.environ['authd']),
+    "databaseURL": str(os.environ['dataurl']),
+    "projectId": str(os.environ['projectid']),
+    "storageBucket": str(os.environ['storagebkt']),
+    "messagingSenderId": str(os.environ['messagesendid']),
+    "appId": str(os.environ['appid']),
+    "measurementId": str(os.environ['measureid'])
   }
 
-token_public = str(os.environ[YOUR_TELEGRAMBOT_TOKEN])
-owner = str(os.environ[OWNER_USERNAME])
-owner_id_telegram = int(os.environ[OWNER_TELEGRAM_ID])
+token_public = str(os.environ['YOUR_TELEGRAMBOT_TOKEN'])
+owner = str(os.environ['OWNER_USERNAME'])
+owner_id_telegram = int(os.environ['OWNER_TELEGRAM_ID'])
