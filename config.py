741949632facdf0f9e070e154871cb1e4e47da64
Swapnil-Singh99 <swapnilskumars@gmail.com>
2021-06-03 18:20:41 +0530

    added config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+firebaseConfig = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "projectId": "",
+    "storageBucket": "",
+    "messagingSenderId": "",
+    "appId": "",
+    "measurementId": ""
+  }
+
+token_public = YOUR_TELEGRAMBOT_TOKEN
+owner = OWNER_USERNAME
+owner_id_telegram = OWNER_TELEGRAM_ID
