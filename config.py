f45ce3727a29789c54ea1ec07e7fd3f22d433adf
Swapnil-Singh99 <swapnilskumars@gmail.com>
2021-06-03 19:04:44 +0530

    config.py ...

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
+import os
+
 firebaseConfig = {
-    "apiKey": apikey,
-    "authDomain": authd,
-    "databaseURL": dataurl,
-    "projectId": projectid,
-    "storageBucket": storagebkt,
-    "messagingSenderId": messagesendid,
-    "appId": appid,
-    "measurementId": measureid
+    "apiKey": str(os.environ[apikey]),
+    "authDomain": str(os.environ[authd]),
+    "databaseURL": str(os.environ[dataurl]),
+    "projectId": str(os.environ[projectid]),
+    "storageBucket": str(os.environ[storagebkt]),
+    "messagingSenderId": str(os.environ[messagesendid]),
+    "appId": str(os.environ[appid]),
+    "measurementId": str(os.environ[measureid])
   }
 
-token_public = YOUR_TELEGRAMBOT_TOKEN
-owner = OWNER_USERNAME
-owner_id_telegram = OWNER_TELEGRAM_ID
+token_public = str(os.environ[YOUR_TELEGRAMBOT_TOKEN])
+owner = str(os.environ[OWNER_USERNAME])
+owner_id_telegram = int(os.environ[OWNER_TELEGRAM_ID])
